a8c7dc5f7fac3c6103ec59f6a2b0629a04bf5d9e
erik <cornelio.hernandez.erik@gmail.com>
2025-08-06 18:03:42 -0500

    eliminebasura

--- Nuevo documento de texto (6).py
+++
@@ -1,13 +0,0 @@
-# importar requests
-import requests
-
-# realizar una petición GET a la API de NumbersApi
-trivia_fetch = requests.get("http://numbersapi.com/42?json")
-
-# convertir el contenido de la respuesta a formato JSON
-# y almacenarlo en una variabletrivia = trivia_fetch.json()
-print("Información de la trivia", trivia)
-# acceder al valor de la clave 'text' en el JSON
-print("Mensaje de la trivia:", trivia['text'])
-# acceder al valor de la clave 'number' en el JSON
-print("Número de la trivia:", trivia['number'])
